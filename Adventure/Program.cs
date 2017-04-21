14a8035c49a492cabcd569d860767ef74bcfba9b
morkandmindy <gspence2000@gmail.com>
2017-04-21 13:41:00 -0400

    some very basic command parsing, directions only

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.Xml.Serialization;
 
 namespace Adventure
@@ -18,9 +15,101 @@ namespace Adventure
             Game game = (Game)serializer.Deserialize(loadStream);
             loadStream.Close();
 
-            foreach (var room in game.Rooms)
+            int currRoomId = 1;
+
+            while (true)
             {
-                Console.WriteLine(room.LongDescription);
+                Room currRoom = game.Rooms.FirstOrDefault(r => r.Id == currRoomId);
+
+                Console.WriteLine();
+                Console.WriteLine(currRoom.LongDescription);
+                Console.WriteLine();
+                Console.Write(">");
+                string command = Console.ReadLine().ToLower();
+                Console.WriteLine();
+
+                bool commandHandled = false;
+
+                //substitute abbreviations
+                switch (command)
+                {
+                    case "n":
+                        {
+                            command = "north";
+                            break;
+                        }
+
+                    case "s":
+                        {
+                            command = "south";
+                            break;
+                        }
+                    case "e":
+                        {
+                            command = "east";
+                            break;
+                        }
+                    case "w":
+                        {
+                            command = "west";
+                            break;
+                        }
+                    case "ne":
+                        {
+                            command = "northeast";
+                            break;
+                        }
+                    case "nw":
+                        {
+                            command = "northwest";
+                            break;
+                        }
+                    case "se":
+                        {
+                            command = "southeast";
+                            break;
+                        }
+                    case "sw":
+                        {
+                            command = "southwest";
+                            break;
+                        }
+                    case "u":
+                        {
+                            command = "up";
+                            break;
+                        }
+                    case "d":
+                        {
+                            command = "down";
+                            break;
+                        }
+                }
+
+                Directions x;
+                if (Directions.TryParse(command, true, out x))
+                {
+                    foreach (var passage in currRoom.Passages)
+                    {
+                        if (command == passage.Direction.ToString().ToLower())
+                        {
+                            currRoomId = passage.Destination;
+                            commandHandled = true;
+                        }
+                       
+                    }
+                    if (!commandHandled)
+                    {
+                        Console.WriteLine("You cannot move in that direction.");
+                        commandHandled = true;
+                    }
+                }
+
+                if (!commandHandled)
+                {
+                    Console.WriteLine("I did not recognize that command.");
+                }
+                
             }
         }
     }
