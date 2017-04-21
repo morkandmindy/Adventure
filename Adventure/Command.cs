be208b39294a62f855ccd6c6116c586e84a0e98e
morkandmindy <gspence2000@gmail.com>
2017-04-21 19:08:14 -0400

    lil further - won't compile

---
+++ Adventure/Command.cs
@@ -0,0 +1,60 @@
+﻿using AdventureDataModel;
+
+namespace Adventure
+{
+    class Command
+    {
+        public Command(string val)
+        {
+            string[] words = val.Split(' ');
+            string verb = words[0].ToLower();
+            if (words.Length == 2)
+            {
+                string noun = words[1].ToLower();
+            }
+
+            if (verb == "go")
+            {
+                if (IsDirection(noun))
+                {
+                    Directions d;
+                    Directions.TryParse(noun, out d);
+                    Direction = d;
+                }
+                else
+                {
+                    //throw exception with message handing back the user's input
+                }
+            }
+            else if (IsDirection(verb))
+            {
+                Directions d;
+                Directions.TryParse(noun, out d);
+                Direction = d;
+            }
+        }
+
+        public Verbs Verb { get; set; }
+        public Nouns Noun { get; set; }
+        public Directions Direction { get; set; }
+
+
+        private bool IsDirection(string val)
+        {
+            Directions d;
+            return Directions.TryParse(val, true, out d);
+        }
+
+        private bool IsNoun(string val)
+        {
+            Nouns n;
+            return Nouns.TryParse(val, true, out n);
+        }
+
+        private bool IsVerb(string val)
+        {
+            Verbs v;
+            return Verbs.TryParse(val, true, out v);
+        }
+    }
+}
--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -4,113 +4,127 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Xml.Serialization;
 
+using AdventureDataModel;
+
 namespace Adventure
 {
     class Program
     {
         static void Main(string[] args)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(Game));
-            FileStream loadStream = new FileStream(@"..\..\GameData.xml", FileMode.Open, FileAccess.Read);
-            Game game = (Game)serializer.Deserialize(loadStream);
-            loadStream.Close();
+            var game = LoadGame(@"..\..\GameData.xml");
 
+            RunGame(game);
+        }
+
+
+        private static void RunGame(Game game)
+        {
             int currRoomId = 1;
-
-            while (true)
+            while (true) //keep running until Ctrl+C
             {
                 Room currRoom = game.Rooms.FirstOrDefault(r => r.Id == currRoomId);
 
+                ConsoleHelper.WriteLineWordWrap(currRoom.LongDescription);
+                Console.Write("> ");
+
+                Command cmd = new Command(Console.ReadLine().ToLower());
+
                 Console.WriteLine();
-                Console.WriteLine(currRoom.LongDescription);
+
+                //bool commandHandled = false;
+                //Directions x;
+                //if (Directions.TryParse(command, true, out x))
+                //{
+                //    foreach (var passage in currRoom.Passages)
+                //    {
+                //        if (command == passage.Direction.ToString().ToLower())
+                //        {
+                //            currRoomId = passage.Destination;
+                //            commandHandled = true;
+                //        }
+                //    }
+                //    if (!commandHandled)
+                //    {
+                //        Console.WriteLine("You cannot move in that direction.");
+                //        commandHandled = true;
+                //    }
+                //}
+                //if (!commandHandled)
+                //{
+                //    Console.WriteLine("I did not recognize that command.");
+                //}
+
                 Console.WriteLine();
-                Console.Write(">");
-                string command = Console.ReadLine().ToLower();
-                Console.WriteLine();
-
-                bool commandHandled = false;
-
-                //substitute abbreviations
-                switch (command)
-                {
-                    case "n":
-                        {
-                            command = "north";
-                            break;
-                        }
-
-                    case "s":
-                        {
-                            command = "south";
-                            break;
-                        }
-                    case "e":
-                        {
-                            command = "east";
-                            break;
-                        }
-                    case "w":
-                        {
-                            command = "west";
-                            break;
-                        }
-                    case "ne":
-                        {
-                            command = "northeast";
-                            break;
-                        }
-                    case "nw":
-                        {
-                            command = "northwest";
-                            break;
-                        }
-                    case "se":
-                        {
-                            command = "southeast";
-                            break;
-                        }
-                    case "sw":
-                        {
-                            command = "southwest";
-                            break;
-                        }
-                    case "u":
-                        {
-                            command = "up";
-                            break;
-                        }
-                    case "d":
-                        {
-                            command = "down";
-                            break;
-                        }
-                }
-
-                Directions x;
-                if (Directions.TryParse(command, true, out x))
-                {
-                    foreach (var passage in currRoom.Passages)
-                    {
-                        if (command == passage.Direction.ToString().ToLower())
-                        {
-                            currRoomId = passage.Destination;
-                            commandHandled = true;
-                        }
-                       
-                    }
-                    if (!commandHandled)
-                    {
-                        Console.WriteLine("You cannot move in that direction.");
-                        commandHandled = true;
-                    }
-                }
-
-                if (!commandHandled)
-                {
-                    Console.WriteLine("I did not recognize that command.");
-                }
-                
             }
+        }
+
+        private static Game LoadGame(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Game));
+            FileStream loadStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            Game game = (Game)serializer.Deserialize(loadStream);
+            loadStream.Close();
+            return game;
+        }
+
+        private static string ReplaceAbbreviations(string command)
+        {
+            switch (command)
+            {
+                case "n":
+                {
+                    command = "north";
+                    break;
+                }
+
+                case "s":
+                {
+                    command = "south";
+                    break;
+                }
+                case "e":
+                {
+                    command = "east";
+                    break;
+                }
+                case "w":
+                {
+                    command = "west";
+                    break;
+                }
+                case "ne":
+                {
+                    command = "northeast";
+                    break;
+                }
+                case "nw":
+                {
+                    command = "northwest";
+                    break;
+                }
+                case "se":
+                {
+                    command = "southeast";
+                    break;
+                }
+                case "sw":
+                {
+                    command = "southwest";
+                    break;
+                }
+                case "u":
+                {
+                    command = "up";
+                    break;
+                }
+                case "d":
+                {
+                    command = "down";
+                    break;
+                }
+            }
+            return command;
         }
     }
 }
